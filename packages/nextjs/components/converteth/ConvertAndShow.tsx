e34a731ddf2dfd540b41a31cc2d34695da838f4a
tantodefi <tantodefi@protonmail.com>
2024-03-20 21:02:25 -0600

    added copy to clipboard button

--- packages/nextjs/components/converteth/ConvertAndShow.tsx
+++ packages/nextjs/components/converteth/ConvertAndShow.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import ClipboardJS from "clipboard";
 
 const MAJOR_COINS = ["BTC", "ETH", "OP", "MATIC", "MKR"];
 
@@ -43,7 +44,7 @@ export const ConvertAndShow = ({ data }: ConvertAndShowProps) => {
   };
 
   const coinsToAdd = data.filter(coin => !displayedCoins.includes(coin.symbol));
-
+  new ClipboardJS(".btn");
   return (
     <div className="container mx-auto px-4 mt-8">
       <div className="flex flex-col items-center justify-center">
@@ -113,8 +114,34 @@ export const ConvertAndShow = ({ data }: ConvertAndShowProps) => {
                       style={{ width: "8rem", height: "8rem" }}
                     >
                       <p>
-                        {convertedAmounts[symbol]} {symbol}
+                        <span id="number">{convertedAmounts[symbol]}</span> {symbol}
                       </p>
+                      <button
+                        data-clipboard-target="#number"
+                        onClick={() => {
+                          navigator.clipboard.writeText(convertedAmounts[symbol]);
+                        }}
+                        className="btn relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg bg-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button"
+                      >
+                        <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                            className="w-5 h-5 text-white"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+                            ></path>
+                          </svg>
+                        </span>
+                      </button>
                     </div>
                   ),
               )}
